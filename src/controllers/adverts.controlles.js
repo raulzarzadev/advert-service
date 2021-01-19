fa5ea05c37b610986820cc9b8156e4f669eeae92
Raul Zarza <raulzarza.dev@gmail.com>
2021-01-19 13:17:47 -0600

    Actualizando ruta de all adverts#

--- src/controllers/adverts.controlles.js
+++ src/controllers/adverts.controlles.js
@@ -8,7 +8,7 @@ advertsCtrl.getPublishedAdverts = async (req, res) => {
   res.json({ ok: true, adverts });
 };
 
-advertsCtrl.getManagerAdverts = async (req, res) => {
+advertsCtrl.getAllAdverts = async (req, res) => {
   const adverts = await Advert.find();
   res.json({ ok: true, adverts });
 };
--- src/routes/adverts.routes.js
+++ src/routes/adverts.routes.js
@@ -3,7 +3,7 @@ const router = Router();
 const isAuthenticated = require("../authenticatons/jwt.authentication");
 
 const {
-  getManagerAdverts,
+  getAllAdverts,
   getPublishedAdverts,
   getAdvertsByOwner,
   getAdvert,
@@ -20,16 +20,16 @@ router.route("/")
     .get(getPublishedAdverts)
     .post(isAuthenticated, createAdvert);
 
+router.route( "/all")
+    .get(isAuthenticated, getAllAdverts)
+
 router.route("/:id")
-  .get(isAuthenticated, getAdvertsByOwner)
-  .delete(isAuthenticated, deleteAdvert);
+    .get(isAuthenticated, getAdvertsByOwner)
+    .delete(isAuthenticated, deleteAdvert);
 
 router.route("/editar/:id") 
     .put(updateAdvert)
     .get(isAuthenticated, getAdvert);
-
-router.route( "/allAdverts")
-    .get(isAuthenticated, getManagerAdverts)
 
 router.route("/favorite/:userId")
     .post(isAuthenticated, saveFavoriteAdvert)    
